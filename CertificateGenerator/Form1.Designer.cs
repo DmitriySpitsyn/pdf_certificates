f625ac81f7d7153998504145a52a7d5fffa57a8f
Dmitriy Spitsyn <93diman16@gmail.com>
2022-09-11 20:35:14 +0600

    init commit

--- CertificateGenerator/Form1.Designer.cs
+++ CertificateGenerator/Form1.Designer.cs
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.NumCertificate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimeCalibration = new System.Windows.Forms.DateTimePicker();
+            this.ObjectCalibration = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(518, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(242, 180);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Номер Сертификата";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сформировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NumCertificate
+            // 
+            this.NumCertificate.Location = new System.Drawing.Point(138, 102);
+            this.NumCertificate.Name = "NumCertificate";
+            this.NumCertificate.Size = new System.Drawing.Size(198, 20);
+            this.NumCertificate.TabIndex = 3;
+            this.NumCertificate.Text = "00000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Дата Калибровки";
+            // 
+            // dateTimeCalibration
+            // 
+            this.dateTimeCalibration.Location = new System.Drawing.Point(136, 131);
+            this.dateTimeCalibration.Name = "dateTimeCalibration";
+            this.dateTimeCalibration.Size = new System.Drawing.Size(200, 20);
+            this.dateTimeCalibration.TabIndex = 6;
+            // 
+            // ObjectCalibration
+            // 
+            this.ObjectCalibration.Location = new System.Drawing.Point(138, 172);
+            this.ObjectCalibration.Name = "ObjectCalibration";
+            this.ObjectCalibration.Size = new System.Drawing.Size(198, 20);
+            this.ObjectCalibration.TabIndex = 8;
+            this.ObjectCalibration.Text = "Контроллер";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Обьект калибровки";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(24, 277);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(918, 150);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1214, 636);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ObjectCalibration);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimeCalibration);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NumCertificate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Генератор отчетов";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox NumCertificate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimeCalibration;
+        private System.Windows.Forms.TextBox ObjectCalibration;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
 
--- CertificateGenerator/Form1.cs
+++ CertificateGenerator/Form1.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +19,70 @@ namespace CertificateGenerator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PdfReader PDFReader = new PdfReader(Environment.CurrentDirectory + @"\Пример.pdf");
+            string filenamesave = Environment.CurrentDirectory + @"\"+
+                    DateTime.UtcNow.Hour.ToString()
+                    + DateTime.UtcNow.Minute.ToString()
+                    + DateTime.UtcNow.Second.ToString() + ".pdf";
+                FileStream Stream = new FileStream(filenamesave, FileMode.Create, FileAccess.Write);
+
+            PdfStamper PDFStamper = new PdfStamper(PDFReader, Stream);
+            AcroFields acrf = PDFStamper.AcroFields;
+            acrf.SetField("Реквизиты", richTextBox1.Text);
+            acrf.SetField("НомерСертификата", DateTime.Now.Millisecond.ToString());
+            acrf.SetField("ДатаКалибровки", dateTimeCalibration.Value.ToShortDateString());
+            acrf.SetField("ОбьектКалибровки", ObjectCalibration.Text);
+
+
+
+            PDFStamper.Close();
+            PDFReader.Close();
+            Stream.Close();
+            System.Diagnostics.Process.Start(filenamesave);
+            using (SQLiteConnection Connect = new SQLiteConnection(@"Data Source=" + Environment.CurrentDirectory + @"\" + "DbCore.db" + "; Version=3;"))
+            {
+                Connect.Open();
+                SQLiteCommand comm = new SQLiteCommand("Insert INto Certificates Values('" + 
+                    DateTime.Now.Millisecond.ToString()+"','"+
+                    ObjectCalibration.Text + "'," +
+                    "CURRENT_TIMESTAMP"+
+                    ");", Connect);
+                comm.ExecuteNonQuery();
+                Connect.Close();
+
+            }
+
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            using (SQLiteConnection Connect = new SQLiteConnection(@"Data Source=" + Environment.CurrentDirectory + @"\" + "DbCore.db" + "; Version=3;"))
+            {
+                Connect.Open();
+                SQLiteCommand comm = new SQLiteCommand("Select * From Certificates", Connect);
+                using (SQLiteDataReader read = comm.ExecuteReader())
+                {
+                    while (read.Read())
+                    {
+                        dataGridView1.Rows.Add(new object[] {
+            //read.GetValue(0),  // U can use column index
+            read.GetValue(read.GetOrdinal("Number")),  // Or column name like this
+            read.GetValue(read.GetOrdinal("Object")),
+            read.GetValue(read.GetOrdinal("Date"))
+            });
+                    }
+                }
+            }
         }
     }
 }
